35f34c2d048f9ba75d0c8e8a143dbbd35369a255
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-07-31 02:25:54 +0300

    Close adding state for close button and make it work

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
+
 function Announcement() {
+  const [showed, setShowed] = useState<boolean>(true);
+
   return (
-    <div className="bg-grey-10 border-b-grey-20 relative flex h-16 w-full items-center justify-between border-b bg-[url('/images/announcement-bg.png')] bg-cover bg-center font-light text-white md:justify-center md:text-[18px]">
-      <p>
-        ✨ Discover Your Dream Property With Estatein{" "}
-        <a href="" className="block underline md:inline">
-          Learn More
-        </a>
-      </p>
-      <button className="bg-grey-40 absolute right-4 cursor-pointer rounded-full px-2 py-1 opacity-80">
-        &#x2715;
-      </button>
-    </div>
+    showed && (
+      <div className="bg-grey-10 border-b-grey-20 relative flex h-16 w-full items-center justify-between border-b bg-[url('/images/announcement-bg.png')] bg-cover bg-center font-light text-white md:justify-center md:text-[18px]">
+        <p>
+          ✨ Discover Your Dream Property With Estatein{" "}
+          <a href="" className="block underline md:inline">
+            Learn More
+          </a>
+        </p>
+        <button
+          onClick={() => setShowed((show) => !show)}
+          className="bg-grey-40 absolute right-4 cursor-pointer rounded-full px-2 py-1 opacity-80"
+        >
+          &#x2715;
+        </button>
+      </div>
+    )
   );
 }
 
