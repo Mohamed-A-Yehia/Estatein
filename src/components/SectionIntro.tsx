b5599ac1b5f8c2c6b146ba78dcce2e0bec3ccc02
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-02 01:16:37 +0300

    creating SectionsIntro to re-use it (Header & Description)

--- src/components/SectionIntro.tsx
+++ src/components/SectionIntro.tsx
@@ -1,4 +1,4 @@
-import { TiStarFullOutline } from "react-icons/ti";
+import { CiStar } from "react-icons/ci";
 
 type props = {
   title: string;
@@ -9,9 +9,9 @@ function SectionIntro({ title, description }: props) {
   return (
     <>
       <div className="flex items-center">
-        <TiStarFullOutline className="text-grey-40 text-4xl" />
-        <TiStarFullOutline className="text-grey-30 text-3xl" />
-        <TiStarFullOutline className="text-grey-20 text-2xl" />
+        <CiStar className="text-grey-20 text-4xl" />
+        <CiStar className="text-grey-20 text-3xl" />
+        <CiStar className="text-grey-20 text-2xl" />
       </div>
       <div className="mb-10 sm:mb-10">
         <h2 className="my-3 text-2xl font-semibold text-white md:text-4xl">
