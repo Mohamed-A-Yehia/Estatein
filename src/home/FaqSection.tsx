029a235bd0a7727f4af802ec3246b58505fd7b44
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-02 02:47:40 +0300

    creating Faqs section and finish it

--- src/home/FaqSection.tsx
+++ src/home/FaqSection.tsx
@@ -1,12 +1,23 @@
+import { BeatLoader } from "react-spinners";
 import SectionIntro from "../components/SectionIntro";
+import { useFaqs } from "../hooks/useFaqs";
+import Faqs from "./Faqs";
 
 function FaqSection() {
+  const { isLoading, data } = useFaqs();
+
   return (
     <section className="border-grey-15 m-auto border-b p-5 pt-16 pb-1 lg:container">
       <SectionIntro
         title="Frequently Asked Questions"
         description="Find answers to common questions about Estatein's services, property listings, and the real estate process. We're here to provide clarity and assist you every step of the way."
       />
+
+      {isLoading ? (
+        <BeatLoader color="#7520E9" size={50} />
+      ) : (
+        <Faqs faqs={data} />
+      )}
     </section>
   );
 }
--- src/home/Faqs.tsx
+++ src/home/Faqs.tsx
@@ -1,14 +1,40 @@
+import PrevNextButtons from "../components/PrevNextButtons";
+import useDataSize from "../hooks/useDataSize";
+
 interface Faqs {
   question: string;
   answer: string;
 }
 
 type FaqsProps = {
-  faq: Faqs[];
+  faqs: Faqs[];
 };
 
 function Faqs({ faqs }: FaqsProps) {
-  return <div></div>;
+  const { index, maxIndex, start, visibleItems, setIndex } =
+    useDataSize<Faqs>(faqs);
+
+  return (
+    <div>
+      <div className="flex gap-4 transition-all duration-300">
+        {visibleItems.map((faq) => (
+          <div
+            key={faq.question}
+            className="border-grey-20 flex w-full flex-col justify-between gap-3.5 rounded-md border-1 p-6 text-white md:gap-5 lg:w-1/3"
+          >
+            <p className="text-lg font-semibold">{faq.question}</p>
+            <p className="text-grey-60 text-sm font-medium">{faq.answer}</p>
+          </div>
+        ))}
+      </div>
+      <PrevNextButtons
+        maxIndex={maxIndex}
+        setIndex={setIndex}
+        start={start}
+        index={index}
+      />
+    </div>
+  );
 }
 
 export default Faqs;
