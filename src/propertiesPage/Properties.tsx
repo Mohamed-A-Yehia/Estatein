40718f3ec5493d88f46d78a4fea0f4224fbd0566
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-03 21:11:08 +0300

    setting up the inital pages - About us, Properties, Services

---
+++ src/propertiesPage/Properties.tsx
@@ -0,0 +1,9 @@
+function Properties() {
+  return (
+    <>
+      <h1 className="text-center text-9xl text-white">Properties Page</h1>
+    </>
+  );
+}
+
+export default Properties;
---
+++ src/servicesPage/Services.tsx
@@ -0,0 +1,9 @@
+function Services() {
+  return (
+    <>
+      <h1 className="text-center text-9xl text-white">Services Page</h1>
+    </>
+  );
+}
+
+export default Services;
