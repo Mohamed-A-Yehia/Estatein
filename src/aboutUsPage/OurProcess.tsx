c4417598a2ac2c539577a4cdbb9aa6ade98a8b29
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-05 06:09:50 +0300

    finish OurProcess Section

--- src/aboutUsPage/OurProcess.tsx
+++ src/aboutUsPage/OurProcess.tsx
@@ -2,17 +2,17 @@ import SectionIntro from "../components/SectionIntro";
 
 function OurProcess() {
   return (
-    <section className="container m-auto px-4 py-8 md:py-16 xl:px-0">
+    <section className="container m-auto px-4 py-8 md:py-16 lg:px-0 xl:px-10">
       <SectionIntro
         title=" Navigating the Estatein Experience"
         description="At Estatein, we've designed a straightforward process to help you find and purchase your dream property with ease. Here's a step-by-step guide to how it all works."
         stars={true}
       />
 
-      <div className="mt-15 text-white">
-        <div className="grid grid-cols-1 gap-20 md:grid-cols-2 xl:grid-cols-3">
+      <div className="mt-15 grid grid-cols-1 gap-20 text-white md:grid-rows-1">
+        <div className="grid grid-cols-1 gap-20 md:grid-cols-3 md:gap-10 xl:grid-cols-3">
           <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5">
-            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px]">
+            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
               Step 01
             </span>
             <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-11px] before:h-[1px] before:w-20">
@@ -25,7 +25,7 @@ function OurProcess() {
             </p>
           </div>
           <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5">
-            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px]">
+            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
               Step 02
             </span>
             <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-11px] before:h-[1px] before:w-20">
@@ -38,7 +38,7 @@ function OurProcess() {
             </p>
           </div>
           <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5">
-            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px]">
+            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] sm:top-[-15%] xl:top-[-25%]">
               Step 03
             </span>
             <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-11px] before:h-[1px] before:w-20">
@@ -51,7 +51,49 @@ function OurProcess() {
             </p>
           </div>
         </div>
-        <div></div>
+        <div className="hidden md:block">
+          <div className="grid grid-cols-1 gap-20 md:grid-cols-3 md:gap-10 xl:grid-cols-3">
+            <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5">
+              <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
+                Step 04
+              </span>
+              <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-11px] before:h-[1px] before:w-20">
+                See It for Yourself
+              </h4>
+              <p className="text-grey-60 text-[15px]">
+                Arrange viewings of the properties you're interested in. We'll
+                coordinate with the property owners and accompany you to ensure
+                you get a firsthand look at your potential new home.
+              </p>
+            </div>
+            <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5">
+              <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
+                Step 05
+              </span>
+              <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-11px] before:h-[1px] before:w-20">
+                Making Informed Decisions
+              </h4>
+              <p className="text-grey-60 text-[15px]">
+                Before making an offer, our team will assist you with due
+                diligence, including property inspections, legal checks, and
+                market analysis. We want you to be fully informed.
+              </p>
+            </div>
+            <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5">
+              <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
+                Step 06
+              </span>
+              <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-11px] before:h-[1px] before:w-20">
+                Getting The Best Deal
+              </h4>
+              <p className="text-grey-60 text-[15px]">
+                We'll help you negotiate the best terms and prepare your offer.
+                Our goal is to secure the property at the right price and on
+                favorable terms.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
