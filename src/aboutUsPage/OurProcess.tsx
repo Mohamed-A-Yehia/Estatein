cf5b3011e809773f44ebd948569a4f51c718b7b3
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-09 01:29:06 +0300

    Refactor - makes OurProcess clean and scalable

--- src/aboutUsPage/OurProcess.tsx
+++ src/aboutUsPage/OurProcess.tsx
@@ -1,5 +1,66 @@
 import SectionIntro from "../components/SectionIntro";
 
+interface ProcessStep {
+  step: string;
+  title: string;
+  description: string;
+  spanClass: string;
+}
+
+const itemContainerClass =
+  "border-grey-20 relative rounded-lg rounded-tl-none border p-2.5 lg:p-5";
+
+const steps: ProcessStep[] = [
+  {
+    step: "Step 01",
+    title: "Discover a World Of Possibilities",
+    description:
+      "Your journey begins with exploring our carefully curated property listings. Use our intuitive search tools to filter properties based on your preferences, including location,",
+    spanClass:
+      "before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]",
+  },
+  {
+    step: "Step 02",
+    title: "Narrowing Down Your Choices",
+    description:
+      "Once you've found properties that catch your eye, save them to your account or make a shortlist. This allows you to compare and revisit your favorites as you make your decision.",
+    spanClass:
+      "before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]",
+  },
+  {
+    step: "Step 03",
+    title: "Personalized Guidance",
+    description:
+      "Have questions about a property or need more information? Our dedicated team of real estate experts is just a call or message away.",
+    spanClass:
+      "before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] sm:top-[-15%] xl:top-[-25%]",
+  },
+  {
+    step: "Step 04",
+    title: "See It for Yourself",
+    description:
+      "Arrange viewings of the properties you're interested in. We'll coordinate with the property owners and accompany you to ensure you get a firsthand look at your potential new home.",
+    spanClass:
+      "before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]",
+  },
+  {
+    step: "Step 05",
+    title: "Making Informed Decisions",
+    description:
+      "Before making an offer, our team will assist you with due diligence, including property inspections, legal checks, and market analysis. We want you to be fully informed.",
+    spanClass:
+      "before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]",
+  },
+  {
+    step: "Step 06",
+    title: "Getting The Best Deal",
+    description:
+      "We'll help you negotiate the best terms and prepare your offer. Our goal is to secure the property at the right price and on favorable terms.",
+    spanClass:
+      "before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]",
+  },
+];
+
 function OurProcess() {
   return (
     <section className="container m-auto px-4 py-8 md:py-16 xl:px-10">
@@ -11,87 +72,27 @@ function OurProcess() {
 
       <div className="mt-15 grid grid-cols-1 gap-20 pt-5 text-white md:grid-rows-1">
         <div className="grid grid-cols-1 gap-20 md:grid-cols-3 md:gap-10 xl:grid-cols-3">
-          <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5 lg:p-5">
-            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
-              Step 01
-            </span>
-            <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 before:md:ml-[-4%]">
-              Discover a World Of Possibilities
-            </h4>
-            <p className="text-grey-60 text-[15px]">
-              Your journey begins with exploring our carefully curated property
-              listings. Use our intuitive search tools to filter properties
-              based on your preferences, including location,
-            </p>
-          </div>
-          <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5 lg:p-5">
-            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
-              Step 02
-            </span>
-            <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 before:md:ml-[-4%]">
-              Narrowing Down Your Choices
-            </h4>
-            <p className="text-grey-60 text-[15px]">
-              Once you've found properties that catch your eye, save them to
-              your account or make a shortlist. This allows you to compare and
-              revisit your favorites as you make your decision.
-            </p>
-          </div>
-          <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5 lg:p-5">
-            <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] sm:top-[-15%] xl:top-[-25%]">
-              Step 03
-            </span>
-            <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 before:md:ml-[-4%]">
-              Personalized Guidance
-            </h4>
-            <p className="text-grey-60 text-[15px]">
-              Have questions about a property or need more information? Our
-              dedicated team of real estate experts is just a call or message
-              away.
-            </p>
-          </div>
+          {steps.slice(0, 3).map(({ step, title, description, spanClass }) => (
+            <div className={`${itemContainerClass} p-5`} key={step}>
+              <span className={spanClass}>{step}</span>
+              <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 md:text-2xl before:md:ml-[-4%]">
+                {title}
+              </h4>
+              <p className="text-grey-60 text-[15px]">{description}</p>
+            </div>
+          ))}
         </div>
         <div className="hidden md:block">
           <div className="grid grid-cols-1 gap-20 md:grid-cols-3 md:gap-10 xl:grid-cols-3">
-            <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5 lg:p-5">
-              <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
-                Step 04
-              </span>
-              <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 before:md:ml-[-4%]">
-                See It for Yourself
-              </h4>
-              <p className="text-grey-60 text-[15px]">
-                Arrange viewings of the properties you're interested in. We'll
-                coordinate with the property owners and accompany you to ensure
-                you get a firsthand look at your potential new home.
-              </p>
-            </div>
-            <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5 lg:p-5">
-              <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
-                Step 05
-              </span>
-              <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 before:md:ml-[-4%]">
-                Making Informed Decisions
-              </h4>
-              <p className="text-grey-60 text-[15px]">
-                Before making an offer, our team will assist you with due
-                diligence, including property inspections, legal checks, and
-                market analysis. We want you to be fully informed.
-              </p>
-            </div>
-            <div className="border-grey-20 relative rounded-lg rounded-tl-none border p-2.5 lg:p-5">
-              <span className="before:bg-primary-65 absolute top-[-25%] left-0 pl-2.5 before:absolute before:top-[-5px] before:ml-[-11px] before:h-20 before:w-[1px] md:top-[-15%] xl:top-[-25%]">
-                Step 06
-              </span>
-              <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 before:md:ml-[-4%]">
-                Getting The Best Deal
-              </h4>
-              <p className="text-grey-60 text-[15px]">
-                We'll help you negotiate the best terms and prepare your offer.
-                Our goal is to secure the property at the right price and on
-                favorable terms.
-              </p>
-            </div>
+            {steps.slice(3).map(({ step, title, description, spanClass }) => (
+              <div className={`${itemContainerClass} p-5`} key={step}>
+                <span className={spanClass}>{step}</span>
+                <h4 className="before:bg-primary-65 mb-2.5 text-[20px] font-semibold before:absolute before:top-[-1px] before:ml-[-3%] before:h-[1px] before:w-20 md:text-2xl before:md:ml-[-4%]">
+                  {title}
+                </h4>
+                <p className="text-grey-60 text-lg">{description}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
