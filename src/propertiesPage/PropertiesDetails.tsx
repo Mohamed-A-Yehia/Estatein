034fff7c5eff4f32ea8809eabb1ff655fd4afb34
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-09 01:01:55 +0300

    Initial - PropertyPage/propertyId

--- src/propertiesPage/PropertiesDetails.tsx
+++ src/propertiesPage/PropertiesDetails.tsx
@@ -1,7 +1,18 @@
 import { useParams } from "react-router";
+import { useProperties } from "../hooks/useProperties";
+import { BeatLoader } from "react-spinners";
 
 function PropertiesDetails() {
   const { propertyId } = useParams();
+  const { data, isLoading } = useProperties();
+
+  if (isLoading) return <BeatLoader color="#7520E9" size={100} />;
+
+  const selecetedProperty = data?.find(
+    (property) => String(property.id) === propertyId,
+  );
+  console.log(selecetedProperty);
+
   return <h1 className="text-center text-4xl text-white">{propertyId}</h1>;
 }
 
